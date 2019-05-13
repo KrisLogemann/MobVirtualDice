f55b3e2c1a00eacf29686a3c17aada93696811a8
Admin <admin@Admins-Mac-mini.local>
2019-05-12 17:40:00 -0700

    adds color and total
    Co-Authored-By: Taylour, <jayluvsdivn@gmail.com>
    Co-Authored-By: Godfrey, <godmartinz@gmail.com>
    Co-Authored-By: Garrett, <cyph17@gmail.com>
    Co-Authored-By: Gian, <gian.delprado94@gmail.com>
    Co-Authored-By: Kris, <krisrlogemann@gmail.com>
    Co-Authored-By: Brandon, <allthingsrandom@hotmail.com>

--- src/Dice.jsx
+++ src/Dice.jsx
@@ -4,9 +4,35 @@ export default class Dice extends Component{
 
 
     render(){
+        var color;
+        switch (this.props.sides){
+            case '4':
+                color = "text-primary";
+                break;
+
+            case '6':
+                color = "text-secondary";
+                break;
+
+            case '8':
+                color = "text-success";
+                break;
+
+            case '10':
+                color = "text-danger";
+                break;
+
+            case '12':
+                color = "text-warning";
+                break;
+
+            case '20':
+                color = "text-info";
+                break;
+        }
         return (
-            <div className='col well'>
-                <div>{this.props.value} / {this.props.sides}</div>
+            <div className='col well '>
+                <div className={color}>{this.props.value} / {this.props.sides}</div>
             </div>
         );
     }
